67ba3f6a725056f1dc52efdec5e1bbdb90568046
VeroxUniverse <stauplays@gmail.com>
2023-12-14 20:11:43 +0100

    Mana Regen and Max Mana Update

--- src/main/java/net/veroxuniverse/knightsnmages/item/armor/custom/EliteBattleMage.java
+++ src/main/java/net/veroxuniverse/knightsnmages/item/armor/custom/EliteBattleMage.java
@@ -1,5 +1,12 @@
 package net.veroxuniverse.knightsnmages.item.armor.custom;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import com.hollingsworth.arsnouveau.api.perk.IPerk;
+import com.hollingsworth.arsnouveau.api.perk.IPerkHolder;
+import com.hollingsworth.arsnouveau.api.perk.PerkAttributes;
+import com.hollingsworth.arsnouveau.api.perk.PerkInstance;
+import com.hollingsworth.arsnouveau.api.util.PerkUtil;
 import com.hollingsworth.arsnouveau.common.armor.AnimatedMagicArmor;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
@@ -8,6 +15,8 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
@@ -29,6 +38,7 @@ import software.bernie.geckolib.model.GeoModel;
 
 import javax.annotation.Nullable;
 import java.util.Set;
+import java.util.UUID;
 import java.util.function.Consumer;
 
 public class EliteBattleMage extends KNMArmorItem {
@@ -80,6 +90,26 @@ public class EliteBattleMage extends KNMArmorItem {
 
             return isFullSet ? PlayState.CONTINUE : PlayState.STOP;
         }));
+    }
+
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot pEquipmentSlot, ItemStack stack) {
+        ImmutableMultimap.Builder<Attribute, AttributeModifier> attributes = new ImmutableMultimap.Builder<>();
+        attributes.putAll(super.getDefaultAttributeModifiers(pEquipmentSlot));
+        if (this.type.getSlot() == pEquipmentSlot) {
+            UUID uuid = ARMOR_MODIFIER_UUID_PER_TYPE.get(type);
+            IPerkHolder<ItemStack> perkHolder = PerkUtil.getPerkHolder(stack);
+            if (perkHolder != null) {
+                attributes.put(PerkAttributes.MAX_MANA.get(), new AttributeModifier(uuid, "max_mana_armor", 100 * (perkHolder.getTier() + 1), AttributeModifier.Operation.ADDITION));
+                attributes.put(PerkAttributes.MANA_REGEN_BONUS.get(), new AttributeModifier(uuid, "mana_regen_armor", perkHolder.getTier() + 3, AttributeModifier.Operation.ADDITION));
+                for (PerkInstance perkInstance : perkHolder.getPerkInstances()) {
+                    IPerk perk = perkInstance.getPerk();
+                    attributes.putAll(perk.getModifiers(this.type.getSlot(), stack, perkInstance.getSlot().value));
+                }
+
+            }
+        }
+        return attributes.build();
     }
 
 
